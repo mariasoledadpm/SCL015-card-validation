582323381ae6fccc1982750d1778dc5823a96971
mariasoledadpm <maria.pena@usach.cl>
2020-11-03 13:06:55 -0300

    función validación

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
 // declaración de variables a utilizar 
 const primeraPagina = document.getElementById("pagina1");
 const segundaPagina = document.getElementById("pagina2");
@@ -8,9 +8,8 @@ const terceraPagina = document.getElementById("pagina3");
 const cuartaPagina = document.getElementById("pagina4");
 
 //Ocultar segunda página (y tercera y cuarta)
-segundaPagina.style.display= "none";
-terceraPagina.style.display = "none";
-cuartaPagina.style.display= "none";
+
+//segundaPagina.style.display = "none";
 
 //Función para que botón ingresar redirija a segunda pagina para ingresar datos
 const botonIngresar=document.getElementById("ingresar")
@@ -22,18 +21,34 @@ botonIngresar.addEventListener("click", ingresar);
     document.getElementById("pagina2").style.display= "block";
   }
 
-function mostrar(){
-    document.getElementById('pagina2').style.display = 'block';
+  const botonvalidar=document.getElementById("validar")
+  botonvalidar.addEventListener("click", mostrar);  
+
+  function mostrar(){
+//document.getElementById('pagina2').style.display = 'block';
 //segundaPagina.style.display = "block";
 //Crear un evento para mostra y ocultar páginas
-botonIngresar.addEventListener("ingresar",function(){
-primeraPagina.style.display = "none";
-segundaPagina.style.display = "block";
-})
+//botonIngresar.addEventListener("click",mostrar(){
+  document.getElementById("botonvalidar").style.display="none";
+  document.getElementById('pagina1').style.display = "none";
+  document.getElementById("pagina2").style.display= "none";
+  document.getElementById("pagina3").style.display= "block";
+}
+
  //función botón volver a primera pagina
-document.getElementById("volver1").addEventListener("click", ()=>{
-    
-  document.getElementById("pagina1").style.display="none";
-  document.getElementById("pagina2").style.display="block";
+ 
+ 
+  //document.getElementById("volver1").addEventListener("click", ()=>{
+  //document.getElementById("pagina1").style.display="none";
+  //document.getElementById("pagina3").style.display="block";
   
-});
+//});
+//const botonvolver=document.getElementById("volver")
+//botonvolver.addEventListener("click", mostrar);  
+
+//function mostrar(){
+//document.getElementById("botonvolver").style.display="none";
+//document.getElementById('pagina1').style.display = "block";
+//document.getElementById("pagina2").style.display= "none";
+//document.getElementById("pagina3").style.display= "none";
+//}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
 const validator = {
-  // ...
+  invalid:(tarjeta)=>{ 
+    //funcion para validar (valida o invalida)
+    let largoTarjeta= 0;
+    //definimos el largo de la tarjeta 
+    let marcaPosicion= 0;
+    //cuenta desde el inicio considerando el cero
+    let numeroTarjeta= parseInt (tarjeta);
+    //parseint para pasar de string a numeros enteros
+    while (numeroTarjeta !=0) {
+    //while realiza un ciclo 
+    //mientras el numero de tarjeta sea distinto a cero funciona lo del interior
+      let divisionTarjeta= numeroTarjeta%10;
+    //el numeroTarjeta se divide x 10
+      if (marcaPosicion%2!=0) {
+      //le damos una condición que si es par debe ser distinto a cero
+        let multiplicaTarjeta= divisionTarjeta*2;
+      //me toma la posicion par y multiplica x 2
+        if (multiplicaTarjeta >9) {
+        //condición si multiplicaTarjeta es mayor a 9 se ejecuta la variable
+          let multiplicaString= multiplicaTarjeta.toString 
+          //es para sacar los 2 digitos y los separe
+          multiplicaTarjeta= parseInt(multiplicaString.charArt(0)+multiplicaString.charArt(1))
+          //separa los dos numeros que fueron multiplicados en la posicion 2 
+        }
+        largoTarjeta += multiplicaTarjeta 
+        //multiplicaTarjeta es el resultado de la linea 22
+        // al agregar el += se agrega una posicion y a esa posicion 
+      }
+      else{
+        largoTarjeta += divisionTarjeta
+        //si no se cumple la posicion par se agrega a otra posicion
+      }
+      numeroTarjeta= parseInt(numeroTarjeta/10);
+      //el numeroTarjeta se divido por 10 
+      //debo cerrar todas las variables con ;  !!!
+      marcaPosicion ++ ;
+      //le agregamos una posicion 
+    }
+    if (largoTarjeta%10 ==0) {
+      return "Tarjeta Valida"
+    }
+    else {
+      return "Tarjeta Invalida"
+    }
+  }
+
 };
 
 export default validator;
